ea5651434f1c180a5480275ff1c0dd0616b6d0e1
alic3 <alic3dev@gmail.com>
2025-10-08 17:19:35 -0700

    add_fill_screen_parameter

--- include/space_invaders.h
+++ include/space_invaders.h
@@ -1,6 +1,9 @@
 #ifndef __space_invaders_h
 #define __space_invaders_h
 
+extern const unsigned short int space_invaders_renderer_width_default;
+extern const unsigned short int space_invaders_renderer_height_default;
+
 int main(
   int,
   char**
