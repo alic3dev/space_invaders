2b556f5048063c7fc6a3adfa4e2c019e16a1c5f2
alic3 <alic3dev@gmail.com>
2026-05-12 20:53:25 -0700

    `space_invaders_audio_output_io_proc_data`:add

--- include/space_invaders_audio.h
+++ include/space_invaders_audio.h
@@ -1,7 +1,13 @@
 #ifndef __space_invaders_audio_h
 #define __space_invaders_audio_h
 
+#include <space_invaders_game_state.h>
+
 #include <CoreAudio/CoreAudio.h>
+
+struct space_invaders_audio_output_io_proc_data {
+  struct game_state* game_state;
+};
 
 int space_invaders_audio_output_io_proc(
   AudioObjectID,
